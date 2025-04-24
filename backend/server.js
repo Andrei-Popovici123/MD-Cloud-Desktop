f32f1b858023d16cf328463c6951dfaaa104dc70
alexcuruti <alexcuruti77@gmail.com>
2025-04-24 12:18:54 +0300

    Added filename hashing + max size limit of 4 MB

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,11 @@ const cors = require('cors');
 const multer  = require('multer');
 const path = require('path');
 const fs = require('fs');
+const crypto = require('crypto');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -23,15 +24,27 @@ const storage = multer.diskStorage({
     cb(null, 'uploads/')
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
+    const ext = path.extname(file.originalname);
+
+    const hash = crypto.createHash('sha256').update(file.originalname + Date.now().toString() + crypto.randomBytes(16)).digest('hex');
+    
+    //const hash = crypto.randomUUID();
+    cb(null, `${hash}${ext}`);
   }
 })
 
-const upload = multer({ storage: storage })
+const upload = multer({
+  storage: storage,
+   limits: {
+    fileSize: 4 * 1024 * 1024
+  }
+})
 
 
 app.post('/upload', upload.single('file'), function (req, res) {
+  console.log(req.headers);
+  if(!req.file)
+    return res.statusCode(400).json({error:'Upload failed'});
   return res.json({ success: true, originalName: req.file.originalname});
 });
 
