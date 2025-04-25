396ea93ac3b337459ac915bcb5482924bc913ff5
mirunaCalugar <calugarmiruna6@gmail.com>
2025-04-25 20:33:17 +0300

    modification

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const cors = require('cors');
-const multer  = require('multer');
-const path = require('path');
-const fs = require('fs');
-const crypto = require('crypto');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const crypto = require("crypto");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT;
@@ -12,8 +12,7 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-
-const uploadFolder = './uploads/';
+const uploadFolder = "./uploads/";
 
 if (!fs.existsSync(uploadFolder)) {
   fs.mkdirSync(uploadFolder);
@@ -21,33 +20,35 @@ if (!fs.existsSync(uploadFolder)) {
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/')
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
     const ext = path.extname(file.originalname);
 
-    const hash = crypto.createHash('sha256').update(file.originalname + Date.now().toString() + crypto.randomBytes(16)).digest('hex');
-    
+    const hash = crypto
+      .createHash("sha256")
+      .update(
+        file.originalname + Date.now().toString() + crypto.randomBytes(16)
+      )
+      .digest("hex");
+
     //const hash = crypto.randomUUID();
     cb(null, `${hash}${ext}`);
-  }
-})
+  },
+});
 
 const upload = multer({
   storage: storage,
-   limits: {
-    fileSize: 4 * 1024 * 1024
-  }
-})
-
-
-app.post('/upload', upload.single('file'), function (req, res) {
-  console.log(req.headers);
-  if(!req.file)
-    return res.statusCode(400).json({error:'Upload failed'});
-  return res.json({ success: true, originalName: req.file.originalname});
+  limits: {
+    fileSize: 4 * 1024 * 1024,
+  },
 });
 
+app.post("/upload", upload.single("file"), function (req, res) {
+  console.log(req.headers);
+  if (!req.file) return res.statusCode(400).json({ error: "Upload failed" });
+  return res.json({ success: true, originalName: req.file.originalname });
+});
 
 app.listen(PORT, () => {
   console.log(`Express server running at http://localhost:${PORT}/`);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import FileUploader from "./components/FileUploader";
+import MultiScanning from "./components/MultiScanning";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <FileUploader />
+      {/* <MultiScanning /> */}
     </>
   );
 }
