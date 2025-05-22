1f1b21daf9a0deb5b0df8a43ad9a5bd2a720180b
alexcuruti <alexcuruti77@gmail.com>
2025-05-22 13:41:56 +0300

    Improved the windows installation script, added author to the package.json
    To install the app just do .\install.cmd in the root folder

--- install.ps1
+++ install.ps1
@@ -1,4 +1,22 @@
-Write-Host "Installing npm packages..."
+Write-Host "Installing backend npm packages..."
+Set-Location -Path .\backend -PassThru
+npm install
+
+Set-Location -Path .. -PassThru
+
+Write-Host "Installing frontend npm packages..."
+Set-Location -Path .\frontend -PassThru
+npm install
+
+Set-Location -Path .. -PassThru
+
+Write-Host "Creating docker images..."
+docker-compose build
+
+Write-Host "Starting docker containers..."
+docker-compose up -d
+
+Write-Host "Installing electron npm packages..."
 npm install
 
 Write-Host "Building Electron app..."
